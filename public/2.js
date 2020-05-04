8c32f218179da66596c86ff765920fc66fdea39b
Yang, Gyeongpil <ykp9711@gmail.com>
2020-05-04 11:48:32 +0900

    중간구현평가 최종

--- public/2.js
+++
@@ -1,7 +0,0 @@
-let persons1 = [];
-
-    persons1[0] = {name: "홍길동", age: 16  };
-    persons1[1] = {name: "홍길동", age: 17  };
-    console.log(persons1[1]);
-let person2 = persons1;
-console.log(person2[1]);
